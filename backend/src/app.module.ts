f78e17f7233f04758b91a23611b87472ec0b57c5
chuttmateo <chuttmateo0@gmail.com>
2024-05-20 16:20:52 -0300

    adding docker-compose file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3307,
+      username: 'user',
+      password: 'password',
+      database: 'nestjs',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
