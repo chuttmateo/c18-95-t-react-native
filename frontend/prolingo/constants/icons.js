ef74bc2e6d2cb3cc1338ca7183b61483c8d3d97e
SharifAli645 <pandemiaonirica@gmail.com>
2024-05-21 10:04:52 -0500

    Configurar importación inicial de íconos desde assets

---
+++ frontend/prolingo/constants/icons.js
@@ -0,0 +1,9 @@
+import path from '../assets/icons/path.png'
+import rank from "../assets/icons/rank.png";
+import user from "../assets/icons/user.png";
+
+export default {
+    path,  
+    rank,
+    user
+}
