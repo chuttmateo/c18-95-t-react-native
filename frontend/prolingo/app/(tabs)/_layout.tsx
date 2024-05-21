4b0231f495aa4eeeb1b327d5f0c583c9d56d075f
SharifAli645 <pandemiaonirica@gmail.com>
2024-05-21 10:19:18 -0500

    Importar otro componente TabBarIcon

--- frontend/prolingo/app/(tabs)/_layout.tsx
+++ frontend/prolingo/app/(tabs)/_layout.tsx
@@ -1,34 +1,41 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from '@/components/TabBarIcon';
+import icons from '@/constants/icons'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        tabBarShowLabel: false
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Profile',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon  color={color} icon={ icons.user} />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="map" 
         options={{
-          title: 'Explore',
+          title: 'Path',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon color={color} icon={icons.path} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="ranking"
+        options={{
+          title: 'Path',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon color={color} icon={icons.rank} />
           ),
         }}
       />
