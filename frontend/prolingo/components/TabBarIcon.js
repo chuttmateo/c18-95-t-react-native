13cdea44c57c1c3df3ae6a001509362d45a27eaf
SharifAli645 <pandemiaonirica@gmail.com>
2024-05-21 10:43:41 -0500

    Configurar el estilo de los iconos de la barra de pestañas

--- frontend/prolingo/components/TabBarIcon.js
+++ frontend/prolingo/components/TabBarIcon.js
@@ -1,22 +1,18 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-export const TabBarIcon = ({color, icon}) => {
+export const TabBarIcon = ({ color, icon }) => {
+  const styles = StyleSheet.create({
+    image: {
+      width: 40,
+      height: 40,
+      tintColor: color
+    },
+  });
+
   return (
     <View>
-          <Image
-              source={icon}
-              resizeMode='contain'
-              style={ styles.image} />
+      <Image source={icon} resizeMode="contain" style={styles.image} />
     </View>
-  )
-}
-
-
-
-const styles = StyleSheet.create({
-    image: {
-        width: 40,
-        height: 40
-    }
-})
+  );
+};
