a51da63c3f7a7727f55527c54b51f843c7aad885
SharifAli645 <pandemiaonirica@gmail.com>
2024-05-22 13:00:40 -0500

    Configurar importación inicial de íconos e imagenes desde assets para la pestaña de Perfil de usuario

---
+++ frontend/prolingo/constants/profile.js
@@ -0,0 +1,19 @@
+import background3 from "../assets/images/Imgs/backgrounds/backgound3.png";
+import containerLogo from "../assets/images/Imgs/squares & others/circle.png";
+import userData from "../assets/images/Imgs/squares & others/clip1.png";
+import user from "../assets/images/Imgs/squares & others/user.png";
+import edit from "../assets/images/Imgs/icons/editar.png";
+import save from "../assets/images/Imgs/icons/save.png";
+import arrowLeft from "../assets/images/Imgs/bootoms & arrows/arrowLeft.png";
+import arrowRight from "../assets/images/Imgs/bootoms & arrows/arrowRight.png";
+
+export default {
+  background3,
+  containerLogo,
+  userData,
+  user,
+  edit,
+  save,
+  arrowLeft,
+  arrowRight,
+};
