32d49523142873df51f74f57bae6af2f40b530cb
chuttmateo <chuttmateo0@gmail.com>
2024-05-24 16:53:59 -0300

    changes in services

--- backend/src/games/games.service.ts
+++ backend/src/games/games.service.ts
@@ -3,15 +3,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Game } from './entities/game.entity';
 import { CreateGameDto } from './dto/create-game.dto';
-import { Option } from './entities/option.entity';
 
 @Injectable()
 export class GamesService {
   constructor(
     @InjectRepository(Game)
     private gameRepository: Repository<Game>,
-    @InjectRepository(Option)
-    private optionRepository: Repository<Option>,
   ) {}
 
   async findAll(): Promise<Game[]> {
@@ -28,15 +25,7 @@ export class GamesService {
   }
 
   async create(body: CreateGameDto) {
-    const options = body.options.map((optiondto) => {
-      const option = this.optionRepository.create(optiondto);
-      return this.optionRepository.save(option);
-    });
-
-    const game = this.gameRepository.create({
-      ...body,
-      options: await Promise.all(options),
-    });
+    const game = this.gameRepository.create(body);
     return this.gameRepository.save(game);
   }
 
--- backend/src/levels/levels.service.ts
+++ backend/src/levels/levels.service.ts
@@ -4,32 +4,15 @@ import { CreateLevelDto } from './dto/create-level.dto';
 import { Level } from './entities/level.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { LectionsService } from 'src/lections/lections.service';
-import { GamesService } from 'src/games/games.service';
 
 @Injectable()
 export class LevelsService {
   constructor(
     @InjectRepository(Level)
     private readonly repository: Repository<Level>,
-    private readonly lectionsService: LectionsService,
-    private readonly gamesService: GamesService,
   ) {}
   async create(createLevelDto: CreateLevelDto) {
-    // create level
     const level = this.repository.create(createLevelDto);
-    // const lections = createLevelDto.lections.map((lection) => {
-    //   return this.lectionsService.create(lection);
-    // });
-    // const games = createLevelDto.games.map((game) => {
-    //   return this.gamesService.create(game);
-    // });
-
-    // level.lections = await Promise.all(lections);
-    // level.games = await Promise.all(games);
-
-    // save level
-    // above code is not longer needed, using {cascade: true} we can do it automatically
     return this.repository.save(level);
   }
 
