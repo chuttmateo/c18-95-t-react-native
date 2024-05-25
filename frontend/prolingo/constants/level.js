fc9b2b2de2052ffd4b04f62239d1d71d845d2b2b
SharifAli645 <pandemiaonirica@gmail.com>
2024-05-25 09:43:54 -0500

    ADD: Initial setup to use specific assets on the level screen

---
+++ frontend/prolingo/constants/level.js
@@ -0,0 +1,11 @@
+import bg from '../assets/images/Imgs/backgrounds/backgound3_1.png'
+import bg2 from '../assets/images/Imgs/squares & others/clip3.png'
+import header_bg from '../assets/images/Imgs/squares & others/others2.png'
+import arrow from '../assets/images/Imgs/bootoms & arrows/doublearrow.png'
+
+export default {
+    bg,
+    bg2,
+    header_bg,
+    arrow
+} 
