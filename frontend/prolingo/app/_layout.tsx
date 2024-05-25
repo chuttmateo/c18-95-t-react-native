1fc532e82a5a969c8b27041380dcb58f3ac9caca
SharifAli645 <pandemiaonirica@gmail.com>
2024-05-25 10:08:19 -0500

    Delete component that provides a main theme

--- frontend/prolingo/app/_layout.tsx
+++ frontend/prolingo/app/_layout.tsx
@@ -1,24 +1,21 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     Satisfy: require('../assets/fonts/Satisfy-Regular.ttf'),
   });
 
   useEffect(() => {
-    if (loaded) {
+    if (loaded) { 
       SplashScreen.hideAsync();
     }
   }, [loaded]);
@@ -28,11 +25,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+    <Stack screenOptions={{
+        headerShown: false
+      }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="level" />
       </Stack>
-    </ThemeProvider>
   );
 }
