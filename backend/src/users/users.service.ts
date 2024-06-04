bae834316f19ff2331b57ff26b316ce2d2b6bb08
yoelferreyra <yoelferreyra24@gmail.com>
2024-06-04 16:48:59 -0300

    change: findOne per findOneById

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -5,7 +5,6 @@ import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-// This should be a real class/interface representing a user entity
 export type UserVar = any;
 
 @Injectable()
@@ -15,29 +14,12 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
-
-  async findOne(username: string): Promise<UserVar | undefined> {
-    return this.users.find((user) => user.username === username);
+  async findOneById(id: string): Promise<UserVar | undefined> {
+    return this.userRepository.findOne({ where: { id: parseInt(id) } });
   }
 
   async findAll(): Promise<User[]> {
     return this.userRepository.find();
-  }
-
-  async findOneById(id: string): Promise<User | undefined> {
-    return this.userRepository.findOne({ where: { id: parseInt(id, 10) } });
   }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
